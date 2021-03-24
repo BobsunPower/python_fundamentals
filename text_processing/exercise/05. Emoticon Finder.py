92dbd1d53d9f49d6c6e8dc89e5392649414029f5
BobsunPower <bobsunpower@gmail.com>
2021-03-24 17:52:13 +0200

    Completed

--- text_processing/exercise/05. Emoticon Finder.py
+++ text_processing/exercise/05. Emoticon Finder.py
@@ -1,8 +1,8 @@
 # TODO
-# Compile time error
-txt = input()
-emo = []
-for i, c in enumerate(txt):
-    if c == ':':
-        emo.append(txt[i:i + 2])
-print('\n'.join(emo))
+text = input()
+
+for index, char in enumerate(text):
+    emoticon = ""
+    if char == ":" and index + 1 < len(text) and text[index + 1] != " ":
+        emoticon += char + text[index + 1]
+        print(emoticon)
