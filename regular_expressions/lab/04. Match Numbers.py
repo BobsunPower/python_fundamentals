89a8bfd0e4b05d696f26ab37e225018dab9d2abf
BobsunPower <bobsunpower@gmail.com>
2021-03-30 17:16:20 +0300

    Completed

--- regular_expressions/lab/04. Match Numbers.py
+++ regular_expressions/lab/04. Match Numbers.py
@@ -1,18 +1,7 @@
 # TODO
 import re
-
-numbers = input()
-
-pattern = r"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))"
-matches = re.finditer(pattern, numbers)
-
-for match in matches:
-    print(match.group(0), end=" ")
-
-pattern = r"((^|(?<=\s))(-*)(\d+)((\.\d+)*)($|?+\s)))"
-
-matches = re.findall(pattern, numbers)
-
-for match in matches:
-    number, *others = match
-    print(number, end=" ")
+pattern = r'(^|(?<=\s))(?P<num>-?\d+(\.[\d]+)?)($|(?=\s))'
+data = input()
+matches = re.finditer(pattern, data)
+numbers = [n.group('num') for n in matches]
+print(*numbers)
