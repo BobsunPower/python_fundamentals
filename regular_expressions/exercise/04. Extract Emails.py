6d1eafeaf8aa7b2ea9c0799cc377e9e5e82635ce
BobsunPower <bobsunpower@gmail.com>
2021-04-01 22:08:43 +0300

    Completed

---
+++ regular_expressions/exercise/04. Extract Emails.py
@@ -0,0 +1,7 @@
+# TODO
+import re
+text = input()
+pattern = r"(^|(?<=\s))[a-zA-Z\d]+[\._-]?[a-zA-Z\d]+@[a-zA-Z]+\-?[a-zA-Z]+(\.[a-zA-Z]+)+"
+result = re.finditer(pattern, text)
+for element in result:
+    print(element.group())
