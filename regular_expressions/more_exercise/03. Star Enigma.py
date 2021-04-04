ce1337dace1d5d5440c614039defd9c3e879fccf
BobsunPower <bobsunpower@gmail.com>
2021-04-04 22:18:59 +0300

    Completed

--- regular_expressions/more_exercise/03. Star Enigma.py
+++ regular_expressions/more_exercise/03. Star Enigma.py
@@ -1,57 +1,31 @@
-# TODO
 import re
-from collections import defaultdict
 
-planets = defaultdict(list)
-
-
-def decrypt_message(message: str) -> str:
-    """Decrypt message
-    Args:
-        message (str): Message to be decrypted
-    Returns:
-        message (str): Decrypted message
-    """
-    let_pattern = r'(?i)[s,t,a,r]'
-    letters = re.findall(let_pattern, message)
-    factor = len(letters)
-    for i, c in enumerate(message):
-        message = message[:i] + chr(ord(c) - factor) + message[i + 1:]
-    return message
-
-
-def get_message_info(message: str, planets: dict) -> dict:
-    """Add planets to the dictionary
-    Args:
-        message (str): Decrypted message
-        planets (dict): Planets dict
-    Returns:
-        planets (dict): Updated Planets dict
-    """
-    # ! NOT WORKING pattern = r'(?:(?<=\@)(?P<name>[A-Za-z]+)[^@!:>-]*(?<=\:)(?P<population>\d+)[^@!:>-]*(?<=\!)(?P<attack>[AD])(?=\!)[^@!:>-]*(?<=\-\>)(?P<soldiers>[\d]+))'
-    pattern = r'(?:@(?P<name>[A-Za-z]+)[^@!:>-]*:(?P<population>\d+)[^@!:>-]*!(?P<attack>A|D)![^@!:>-]*->(?P<soldiers>[\d]+))'
-    m = re.search(pattern, message)
-
-    if m:
-        if m.group('attack') == 'A':
-            planets['Attacked'].append(m.group('name'))
-        if m.group('attack') == 'D':
-            planets['Destroyed'].append(m.group('name'))
-    return planets
-
-
-n = int(input())
-
-for _ in range(n):
-    message = input()
-    decrypted = decrypt_message(message)
-    planets = get_message_info(decrypted, planets)
-
-print(f'Attacked planets: {len(planets["Attacked"])}')
-if planets['Attacked']:
-    for attacked in sorted(planets['Attacked']):
-        print(f'-> {attacked}')
-print(f'Destroyed planets: {(len(planets["Destroyed"]))}')
-if planets['Destroyed']:
-    for destroyed in sorted(planets['Destroyed']):
-        print(f'-> {destroyed}')
+planets = {'attacked': [], 'destroyed': []}
+lines = int(input())
+for line in range(lines):
+    encrypted_message = input()
+    star_pattern = r"[starSTAR]"
+    key = len(re.findall(star_pattern, encrypted_message))
+    decyphered_message = ""
+    for el in encrypted_message:
+        temp_key = ord(el)
+        new_el = chr(temp_key - key)
+        decyphered_message += new_el
+    planet_pattern = r"(?<=@)(?P<planet>[A-Za-z]+)[^@!:>-]*:(?P<population>\d+)[^@!:>-]*(!(?P<attack_destruct>A|D)!)[" \
+                     r"^@!:>-]*(->(?P<soldiers>\d+)) "
+    planet = [obj.groupdict() for obj in re.finditer(planet_pattern, decyphered_message)]
+    if planet:
+        if planet[0]['attack_destruct'] == "A":
+            planets['attacked'].append(planet[0]['planet'])
+        else:
+            planets['destroyed'].append(planet[0]['planet'])
+for planet in planets.values():
+    planet.sort()
+print(f"Attacked planets: {len(planets['attacked'])}")
+if planets['attacked']:
+    for planet in planets['attacked']:
+        print(f"-> {planet}")
+print(f"Destroyed planets: {len(planets['destroyed'])}")
+if planets['destroyed']:
+    for planet in planets['destroyed']:
+        print(f"-> {planet}")
