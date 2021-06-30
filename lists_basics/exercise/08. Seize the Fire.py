fc508b43fd8f8dfc5a4b826c30bebadb731c4e4f
BobsunPower <bobsunpower@gmail.com>
2021-06-30 10:45:19 +0300

    Completed

--- lists_basics/exercise/08. Seize the Fire.py
+++ lists_basics/exercise/08. Seize the Fire.py
@@ -1,24 +1,17 @@
 lst = input().split("#")
 lst = [i.split(" = ") for i in lst]
-water = int(input())
-effort = 0
-fire = 0
-cells = []
-valid = False
-for i in range(len(lst)):
-    level = lst[i][0]
-    value = int(lst[i][1])
-    valid = (
-        (level == "High" and 81 <= value <= 126) or
-        (level == "Medium" and 51 <= value <= 80) or
-        (level == "Low" and 1 <= value <= 50)
-    )
-    if valid and water >= value:
-        water -= value
-        effort += value * 0.25
-        fire += value
-        cells.append(value)
+wtr, eft, fir = int(input()), 0, 0
 print("Cells:")
-for cell in cells:
-    print(f" - {cell}")
-print(f"Effort: {effort:.2f}\nTotal Fire: {fire}")
+for i in range(len(lst)):
+    lvl, vlu = lst[i][0], int(lst[i][1])
+    vld = (
+        (lvl == "High" and vlu in range(81, 126) and wtr >= vlu) or
+        (lvl == "Medium" and vlu in range(51, 81) and wtr >= vlu) or
+        (lvl == "Low" and vlu in range(1, 51) and wtr >= vlu)
+    )
+    if vld:
+        wtr -= vlu
+        eft += vlu * 0.25
+        fir += vlu
+        print(f" - {vlu}")
+print(f"Effort: {eft:.2f}\nTotal Fire: {fir}")
