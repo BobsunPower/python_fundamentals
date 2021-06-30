066b3d6c80a4c419cd0316ef57d07c09be897fb4
BobsunPower <bobsunpower@gmail.com>
2021-06-30 11:23:11 +0300

    Completed

--- lists_basics/exercise/09. Hello, France.py
+++ lists_basics/exercise/09. Hello, France.py
@@ -1,21 +1,16 @@
-lst = input().split("|")
-budget = float(input())
-sales = []
-profit = []
-for i in lst:
-    item, price = i.split("->")
-    price = float(price)
-    if budget >= price:
-        if item == 'Clothes' and price <= 50.00 \
-                or item == 'Shoes' and price <= 35.00 \
-                or item == 'Accessories' and price <= 20.50:
-            sales.append(price * 1.4)
-            profit.append(price * 0.4)
-            budget -= price
-for j in sales:
-    print(f"{j:.2f}", end=" ")
-print(f"\nProfit: {sum(profit):.2f}")
-if budget + sum(sales) >= 150:
-    print("Hello, France!")
-else:
-    print("Time to go.")
+lst, bgt, pft, sls = input().split("|"), float(input()), 0, 0
+lst = [i.split("->") for i in lst]
+for i in range(len(lst)):
+    typ, vlu = lst[i][0], float(lst[i][1])
+    vld = (
+        (typ == "Clothes" and vlu <= 50 and bgt >= vlu) or
+        (typ == "Shoes" and vlu <= 35 and bgt >= vlu) or
+        (typ == "Accessories" and vlu <= 20.5 and bgt >= vlu)
+    )
+    if vld:
+        bgt -= vlu
+        pft += vlu * 0.4
+        sls += vlu * 1.4
+        print(f"{vlu * 1.4:.2f}", end=" ")
+print(f"\nProfit: {pft:.2f}")
+print("Hello, France!") if bgt + sls >= 150 else print("Time to go.")
