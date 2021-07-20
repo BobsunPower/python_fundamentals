b4884ab27831eea5da2e416bcacecb7af65b26cf
BobsunPower <bobsunpower@gmail.com>
2021-07-20 15:12:59 +0300

    Completed

--- lists_advanced/lab/04. Palindrome Strings.py
+++ lists_advanced/lab/04. Palindrome Strings.py
@@ -1,3 +1,2 @@
-lst = input().split()
-palindrome = input()
-print(f"{[i for i in lst if i == i[::-1]]}\nFound palindrome {lst.count(palindrome)} times")
+lst, pld = input().split(), input()
+print(f"{[i for i in lst if i == i[::-1]]}\nFound palindrome {lst.count(pld)} times")
