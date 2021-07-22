d03c4085bc6f3192f3a42387415e2679a6ffbd33
BobsunPower <bobsunpower@gmail.com>
2021-07-22 17:04:17 +0300

    Completed

--- lists_advanced/exercise/09. Moving Target.py
+++ lists_advanced/exercise/09. Moving Target.py
@@ -1,26 +1,21 @@
-tgt = list(map(lambda x: int(x), input().split()))
-cmd = input()
-while cmd != 'End':
-    comm = cmd.split()
-    command = comm[0]
-    idx = int(comm[1])
-    pwr = int(comm[2])
-    if command == 'Shoot':
-        if 0 <= idx < len(tgt):
-            tgt[idx] -= pwr
-            if tgt[idx] <= 0:
-                tgt.remove(tgt[idx])
-    elif command == 'Add':
-        if 0 <= idx < len(tgt):
-            tgt.insert(idx, pwr)
+lst = list(map(int, input().split()))
+while True:
+    cmd = input().split()
+    if cmd[0] == "End":
+        break
+    idx, vlu = int(cmd[1]), int(cmd[2])
+    if cmd[0] == "Shoot":
+        if 0 <= idx < len(lst):
+            lst[idx] -= vlu
+            lst.remove(lst[idx]) if lst[idx] < 1 else lst[idx]
+    elif cmd[0] == "Add":
+        if 0 <= idx < len(lst):
+            lst.insert(idx, vlu)
         else:
             print("Invalid placement!")
-    else:
-        range_shot = pwr
-        if idx + range_shot < len(tgt)-1 and idx - range_shot >= 0:
-            del tgt[idx - range_shot:idx + range_shot + 1]
+    elif cmd[0] == "Strike":
+        if idx + vlu < len(lst) and idx - vlu >= 0:
+            del lst[idx - vlu:idx + vlu + 1]
         else:
             print("Strike missed!")
-    cmd = input()
-list_to_print = [str(i) for i in tgt]
-print('|'.join(list_to_print))
+print("|".join([str(i) for i in lst]))
