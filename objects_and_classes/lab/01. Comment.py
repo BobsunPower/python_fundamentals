918b7965f373ce9c59473e2a9d23d1a485e9d728
BobsunPower <bobsunpower@gmail.com>
2021-07-23 00:46:08 +0300

    Completed

--- objects_and_classes/lab/01. Comment.py
+++ objects_and_classes/lab/01. Comment.py
@@ -1,5 +1,3 @@
 class Comment:
     def __init__(self, username, content, likes=0):
-        self.username = username
-        self.content = content
-        self.likes = likes
+        self.username, self.content, self.likes = username, content, likes
