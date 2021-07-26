01bae75ad81c203bcbd362cbc1343caf92d01382
BobsunPower <bobsunpower@gmail.com>
2021-07-26 23:52:39 +0300

    Completed

--- objects_and_classes/exercise/05. Class.py
+++ objects_and_classes/exercise/05. Class.py
@@ -2,18 +2,14 @@ class Class:
     __students_count = 22
 
     def __init__(self, name):
-        self.name = name
-        self.students = []
-        self.grades = []
+        self.name, self.students, self.grades = name, [], []
 
-    def add_student(self, name, grade):
-        if Class.__students_count > 0:
-            self.students.append(name)
-            self.grades.append(grade)
-            Class.__students_count -= 1
+    def add_student(self, name: str, grade: float):
+        if len(self.students) < Class.__students_count:
+            self.students.append(name), self.grades.append(grade)
 
     def get_average_grade(self):
-        return float(f"{sum(self.grades) / len(self.grades):.2f}")
+        return sum(self.grades) / len(self.grades)
 
     def __repr__(self):
-        return f"The students in {self.name}: {', '.join(self.students)}. Average grade: {self.get_average_grade()}"
+        return f"The students in {self.name}: {', '.join(self.students)}. Average grade: {self.get_average_grade():.2f}"
