e5617b28a2f2b9fd098c32fa19c3a216a42dab90
BobsunPower <bobsunpower@gmail.com>
2021-07-28 00:06:58 +0300

    Completed

--- dictionaries/lab/02. Stock.py
+++ dictionaries/lab/02. Stock.py
@@ -1,9 +1,4 @@
-data = input().split()
-goods = {data[i]: int(data[i + 1]) for i in range(0, len(data), 2)}
-lst = input().split()
-for itm in lst:
-    if itm in goods:
-        vlu = goods[itm],
-        print(f"We have {vlu} of {itm} left")
-    else:
-        print(f"Sorry, we don't have {itm}")
+lst, sch = input().split(), input().split()
+dic = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
+for i in sch:
+    print(f"We have {dic[i]} of {i} left") if i in dic else print(f"Sorry, we don't have {i}")
