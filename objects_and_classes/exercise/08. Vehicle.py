9ca6464b2b31c46302a0309ad13a72818ad33d25
BobsunPower <bobsunpower@gmail.com>
2021-07-27 18:23:17 +0300

    Completed

--- objects_and_classes/exercise/08. Vehicle.py
+++ objects_and_classes/exercise/08. Vehicle.py
@@ -1,29 +1,21 @@
 class Vehicle:
-    def __init__(self, typ, model, price):
-        self.type = typ
-        self.model = model
-        self.price = price
-        self.owner = None
+    def __init__(self, type, model, price):
+        self.type, self.model, self.price, self.owner = type, model, price, None
 
     def buy(self, money, owner):
+        if self.price > money:
+            return "Sorry, not enough money"
         if self.owner:
             return "Car already sold"
-        else:
-            if money < self.price:
-                return "Sorry, not enough money"
-            else:
-                self.owner = owner
-                change = f"{money - self.price:.2f}"
-                return f"Successfully bought a {self.type}. Change: {change}"
+        self.owner = owner
+        return f"Successfully bought a {self.type}. Change: {money - self.price:.2f}"
 
     def sell(self):
         if self.owner:
             self.owner = None
-        else:
-            return "Vehicle has no owner"
+        return "Vehicle has no owner"
 
     def __repr__(self):
         if self.owner:
             return f"{self.model} {self.type} is owned by: {self.owner}"
-        else:
-            return f"{self.model} {self.type} is on sale: {self.price}"
+        return f"{self.model} {self.type} is on sale: {self.price}"
--- objects_and_classes/exercise/09. Movie.py
+++ objects_and_classes/exercise/09. Movie.py
@@ -1,24 +1,19 @@
 class Movie:
     __watched_movies = []
-    __total_watched = 0
 
     def __init__(self, name, director):
-        self.name = name
-        self.director = director
-        self.watched = False
+        self.name, self.director, self.watched = name, director, False
 
-    def change_name(self, name):
-        self.name = name
+    def change_name(self, new_name):
+        self.name = new_name
 
-    def change_director(self, director):
-        self.director = director
+    def change_director(self, new_director):
+        self.director = new_director
 
     def watch(self):
-        if self.name not in self.__watched_movies:
-            self.watched = True
-            Movie.__total_watched += 1
+        if self.name not in Movie.__watched_movies:
             Movie.__watched_movies.append(self.name)
+            self.watched = True
 
     def __repr__(self):
-        return \
-            f"Movie name: {self.name}; Movie director: {self.director}. Total watched movies: {Movie.__total_watched}"
+        return f"Movie name: {self.name}; Movie director: {self.director}. Total watched movies: {len(self.__watched_movies)}"
