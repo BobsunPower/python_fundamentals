46b875a58777a67384afe543846fd9206dd03a86
BobsunPower <bobsunpower@gmail.com>
2021-08-01 00:35:44 +0300

    Completed

--- dictionaries/exercise/08. Student Academy.py
+++ dictionaries/exercise/08. Student Academy.py
@@ -4,6 +4,7 @@ dic, out, std = defaultdict(list), defaultdict(list), None
 for i in range(int(input())):
     std, grd = input(), float(input())
     dic[std].append(grd)
-[out[std].append(mean(i)) for i in dic.values() if mean(i) >= 4.5]
-[print(f"{name} -> {mean(res):.2f}") for name, res in sorted(out.items(), key=lambda x: x[1], reverse=True)]
-
+for i, j in dic.items():
+    if mean(j) >= 4.5:
+        out[i] = mean(j)
+[print(f"{i} -> {j:.2f}") for i, j in sorted(out.items(), key=lambda x: x[1], reverse=True)]
