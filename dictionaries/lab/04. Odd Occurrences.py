b323dc0fcd06a5a86d1aba9de57979e67a20a590
BobsunPower <bobsunpower@gmail.com>
2021-09-08 19:41:51 +0300

    Completed

--- dictionaries/lab/04. Odd Occurrences.py
+++ dictionaries/lab/04. Odd Occurrences.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-wds, dic = input().split(), defaultdict(int)
-for i in wds:
+lst, dic = input().split(), defaultdict(int)
+for i in lst:
     dic[i.lower()] += 1
 print(" ".join([i for i, j in dic.items() if j % 2 == 1]))
