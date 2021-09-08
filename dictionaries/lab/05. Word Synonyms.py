ccf1b36b6230ddcdee0c61eaa50029088fb15b4f
BobsunPower <bobsunpower@gmail.com>
2021-09-08 23:20:23 +0300

    Completed

--- dictionaries/lab/05. Word Synonyms.py
+++ dictionaries/lab/05. Word Synonyms.py
@@ -1,7 +1,5 @@
 from collections import defaultdict
 dic = defaultdict(list)
-for i in range(int(input())):
-    wrd, syn = input(), input()
-    dic[wrd].append(syn)
+[dic[input()].append(input()) for i in range(int(input()))]
 for i, j in dic.items():
     print(f"{i} - {', '.join(j)}")
