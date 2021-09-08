613d7fdc82c040b0a14c3bc99bcf59c0f47ab3e8
BobsunPower <bobsunpower@gmail.com>
2021-09-08 14:53:08 +0300

    Completed

--- dictionaries/lab/03. Statistics.py
+++ dictionaries/lab/03. Statistics.py
@@ -4,7 +4,7 @@ while True:
     cmd = input().split(": ")
     if cmd[0] == "statistics":
         break
-    itm, qty = cmd[0], int(cmd[1])
-    dic[itm] += int(qty)
+    itm, vlu = cmd[0], int(cmd[1])
+    dic[itm] += vlu
 print("Products in stock:"), [print(f"- {i}: {j}") for i, j in dic.items()]
 print(f"Total Products: {len(dic)}\nTotal Quantity: {sum(dic.values())}")
