5605d607708695bd7d875422f259c73c441dd239
BobsunPower <bobsunpower@gmail.com>
2021-09-09 23:15:47 +0300

    Completed

--- dictionaries/lab/06. Students.py
+++ dictionaries/lab/06. Students.py
@@ -1,15 +1,11 @@
-dic, ctr = {}, 0
+from collections import defaultdict
+dic = defaultdict(list)
 while True:
     cmd = input()
     if len(cmd.split(":")) < 3:
-        cmd.split("_")
-        for i in dic.values():
-            omg = i[0].split()
-            omg = omg[0]
-            if omg in cmd:
-                print(f"{i[1][0]} - {i[1][1]}")
+        cmd = " ".join(cmd.split("_"))
+        for i in dic[cmd]:
+            print(f"{i[0]} - {i[1]}")
         break
-    std, idn, crs = cmd.split(":")
-    new = [crs, [std, idn]]
-    dic[ctr] = new
-    ctr += 1
+    std, num, crs = cmd.split(":")
+    dic[crs].append([std, num])
