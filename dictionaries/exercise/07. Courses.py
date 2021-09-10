21db65c8a027b2fa957ce71d0fdf8cc7384766e3
BobsunPower <bobsunpower@gmail.com>
2021-09-10 22:58:40 +0300

    Completed

--- dictionaries/exercise/07. Courses.py
+++ dictionaries/exercise/07. Courses.py
@@ -1,16 +1,13 @@
-subj = {}
+from collections import defaultdict
+dic = defaultdict(list)
 while True:
     cmd = input()
     if cmd == "end":
         break
     crs, std = cmd.split(" : ")
-    if crs not in subj:
-        subj[crs] = [std]
-    else:
-        subj[crs].append(std)
+    dic[crs].append(std)
 out = []
-for crs, students in sorted(subj.items(), key=lambda x: -len(x[1])):
+for crs, students in sorted(dic.items(), key=lambda x: -len(x[1])):
     out.append(f'{crs}: {len(students)}')
-    for std in sorted(students):
-        out.append(f'-- {std}')
+    [out.append(f'-- {std}') for std in sorted(students)]
 print('\n'.join(out))
