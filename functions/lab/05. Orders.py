85964953fdcf4de98a7be883fd9b02a8d12c971c
BobsunPower <bobsunpower@gmail.com>
2021-12-28 11:59:01 +0200

    Completed

--- functions/lab/05. Orders.py
+++ functions/lab/05. Orders.py
@@ -1,11 +1,11 @@
-def orders(itm, qty):
-    if itm == "coffee":
-        return f"{1.5 * qty:.2f}"
-    elif itm == "water":
+def orders(pdt, qty):
+    if pdt == "coffee":
+        return f"{1.5 * qty: .2f}"
+    if pdt == "water":
         return f"{qty:.2f}"
-    elif itm == "coke":
+    if pdt == "coke":
         return f"{1.4 * qty:.2f}"
-    elif itm == "snacks":
+    if pdt == "snacks":
         return f"{2 * qty:.2f}"
 
 
