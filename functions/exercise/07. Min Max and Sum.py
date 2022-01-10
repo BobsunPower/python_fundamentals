6185adf6436bc2f48dd5ca52ca062ea08829795c
BobsunPower <bobsunpower@gmail.com>
2022-01-10 15:41:57 +0200

    Completed

---
+++ functions/exercise/07. Min Max and Sum.py
@@ -0,0 +1,5 @@
+def min_max_sum(lst):
+    print(f"The minimum number is {min(lst)}\nThe maximum number is {max(lst)}\nThe sum number is: {sum(lst)}")
+
+
+min_max_sum([int(i) for i in input().split()])
