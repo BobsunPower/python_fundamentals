bbfed1fadd9279478b5155c436cd659ce771c0e0
BobsunPower <bobsunpower@gmail.com>
2022-01-11 15:14:56 +0200

    Completed

--- lists_advanced/lab/01. No Vowels.py
+++ lists_advanced/lab/01. No Vowels.py
@@ -1,2 +1,1 @@
-lst = list(input())
-print("".join([i for i in lst if i != "a" and i != "o" and i != "u" and i != "e" and i != "i"]))
+print("".join([i for i in list(input()) if i != "a" and i != "o" and i != "u" and i != "e" and i != "i"]))
--- lists_advanced/lab/02. Trains.py
+++ lists_advanced/lab/02. Trains.py
@@ -2,11 +2,11 @@ lst = [0] * int(input())
 while True:
     cmd = input().split()
     if cmd[0] == "End":
+        print(lst)
         break
     if cmd[0] == "add":
         lst[-1] += int(cmd[1])
-    elif cmd[0] == "insert":
+    if cmd[0] == "insert":
         lst[int(cmd[1])] += int(cmd[2])
-    elif cmd[0] == "leave":
+    if cmd[0] == "leave":
         lst[int(cmd[1])] -= int(cmd[2])
-print(lst)
