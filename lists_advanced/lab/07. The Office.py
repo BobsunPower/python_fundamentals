2e3c82bc78394e41f5a8f552c07e62c8aba225ae
BobsunPower <bobsunpower@gmail.com>
2022-01-12 16:52:02 +0200

    Completed

--- lists_advanced/lab/07. The Office.py
+++ lists_advanced/lab/07. The Office.py
@@ -1,6 +1,6 @@
-emp, fac = list(map(int, input().split())), int(input())
-hpy = len([i * fac for i in emp if i >= sum(emp) / len(emp)])
-if hpy >= len(emp) / 2:
-    print(f"Score: {hpy}/{len(emp)}. Employees are happy!")
+lst, fac = list(map(int, input().split())), int(input())
+hpy = len([i * fac for i in lst if i >= sum(lst) / len(lst)])
+if hpy >= (len(lst) / 2):
+    print(f"Score: {hpy}/{len(lst)}. Employees are happy!")
 else:
-    print(f"Score: {hpy}/{len(emp)}. Employees are not happy!")
+    print(f"Score: {hpy}/{len(lst)}. Employees are not happy!")
