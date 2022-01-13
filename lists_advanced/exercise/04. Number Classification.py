7a39fd620efc91a7dadc31c16155c18053bd06dd
BobsunPower <bobsunpower@gmail.com>
2022-01-13 19:44:54 +0200

    Completed

--- lists_advanced/exercise/04. Number Classification.py
+++ lists_advanced/exercise/04. Number Classification.py
@@ -1,5 +1,5 @@
 lst = list(map(int, input().split(", ")))
-print(f"Positive: " + ", ".join([str(i) for i in lst if i >= 0]))
-print(f"Negative: " + ", ".join([str(i) for i in lst if i < 0]))
-print(f"Even: " + ", ".join([str(i) for i in lst if i % 2 == 0]))
-print(f"Odd: " + ", ".join([str(i) for i in lst if i % 2 == 1]))
+print("Positive: " + ", ".join([str(i) for i in lst if i >= 0]),
+      "\nNegative: " + ", ".join([str(i) for i in lst if i < 0]),
+      "\nEven: " + ", ".join([str(i) for i in lst if i % 2 == 0]),
+      "\nOdd: " + ", ".join([str(i) for i in lst if i % 2 == 1]))
