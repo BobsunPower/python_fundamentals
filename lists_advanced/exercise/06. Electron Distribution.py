e958bc50ef9b5f5cb254792e5a4e79ee91e74773
BobsunPower <bobsunpower@gmail.com>
2022-01-14 18:30:11 +0200

    Completed

--- lists_advanced/exercise/06. Electron Distribution.py
+++ lists_advanced/exercise/06. Electron Distribution.py
@@ -1,10 +1,7 @@
-ets, out, fac = int(input()), [], 0
-while ets > 0:
+els, lst, fac = int(input()), [], 0
+while els > 0:
     fac += 1
     vlu = 2 * fac ** 2
-    if vlu <= ets:
-        out.append(vlu)
-    else:
-        out.append(ets)
-    ets -= vlu
-print(out)
+    lst.append(vlu) if vlu <= els else lst.append(els)
+    els -= vlu
+print(lst)
