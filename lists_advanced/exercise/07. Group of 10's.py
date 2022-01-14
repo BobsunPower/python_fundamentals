af988597f22d21fef720a1b7435dba8f29bbe67e
BobsunPower <bobsunpower@gmail.com>
2022-01-14 19:21:10 +0200

    Completed

--- lists_advanced/exercise/07. Group of 10's.py
+++ lists_advanced/exercise/07. Group of 10's.py
@@ -1,5 +1,4 @@
 from math import ceil
 
 lst = list(map(int, input().split(", ")))
-for i in range((ceil(max(lst) / 10))):
-    print(f"Group of {i + 1}0's: {[j for j in lst if i * 10 < j <= (i + 1) * 10]}")
+[print(f"Group of {i + 1}0's: {[j for j in lst if i * 10 < j <= (i + 1) * 10]}") for i in range((ceil(max(lst) / 10)))]
