2acec258aeadfc79c6ca2af7043846892ce9c1ac
BobsunPower <bobsunpower@gmail.com>
2022-01-17 18:56:31 +0200

    Completed

--- lists_advanced/exercise/10. Heart Delivery.py
+++ lists_advanced/exercise/10. Heart Delivery.py
@@ -6,13 +6,10 @@ while True:
     i += int(cmd[1])
     i = 0 if i >= len(lst) else i
     lst[i] -= 2
-    if lst[i] < 0:
-        print(f"Place {i} already had Valentine's day.")
+    lst[i] < 0 and print(f"Place {i} already had Valentine's day.")
     if lst[i] == 0:
         print(f"Place {i} has Valentine's day.")
         vtn += 1
 print(f"Cupid's last position was {i}.")
-if len(lst) == vtn:
-    print("Mission was successful.")
-else:
-    print(f"Cupid has failed {len(lst) - vtn} places.")
+len(lst) == vtn and print("Mission was successful.")
+len(lst) != vtn and print(f"Cupid has failed {len(lst) - vtn} places.")
