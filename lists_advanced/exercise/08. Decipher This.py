735e92448c1b962bb016bce78262f37a03bad188
BobsunPower <bobsunpower@gmail.com>
2022-01-17 16:30:53 +0200

    Completed

--- lists_advanced/exercise/08. Decipher This.py
+++ lists_advanced/exercise/08. Decipher This.py
@@ -1,5 +1,4 @@
-lst = input().split()
-for i in lst:
+for i in input().split():
     msg = list(chr(int("".join([j for j in i if j.isdigit()])))) + [j for j in i if j.isalpha()]
     msg[1], msg[-1] = msg[-1], msg[1]
     print("".join(msg), end=" ")
