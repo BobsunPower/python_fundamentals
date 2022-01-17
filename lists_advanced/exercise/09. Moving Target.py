007bca16cea5a9a035472e38a26094d1da556f2b
BobsunPower <bobsunpower@gmail.com>
2022-01-17 17:53:36 +0200

    Completed

--- lists_advanced/exercise/09. Moving Target.py
+++ lists_advanced/exercise/09. Moving Target.py
@@ -2,20 +2,17 @@ lst = list(map(int, input().split()))
 while True:
     cmd = input().split()
     if cmd[0] == "End":
+        print("|".join([str(i) for i in lst]))
         break
-    idx, vlu = int(cmd[1]), int(cmd[2])
-    if cmd[0] == "Shoot":
+    act, idx, vlu = cmd[0], int(cmd[1]), int(cmd[2])
+    if act == "Shoot":
         if 0 <= idx < len(lst):
             lst[idx] -= vlu
             lst.remove(lst[idx]) if lst[idx] < 1 else lst[idx]
-    elif cmd[0] == "Add":
-        if 0 <= idx < len(lst):
-            lst.insert(idx, vlu)
-        else:
-            print("Invalid placement!")
-    elif cmd[0] == "Strike":
+    elif act == "Add":
+        lst.insert(idx, vlu) if 0 <= idx < len(lst) else print("Invalid placement!")
+    elif act == "Strike":
         if idx + vlu < len(lst) and idx - vlu >= 0:
             del lst[idx - vlu:idx + vlu + 1]
         else:
             print("Strike missed!")
-print("|".join([str(i) for i in lst]))
