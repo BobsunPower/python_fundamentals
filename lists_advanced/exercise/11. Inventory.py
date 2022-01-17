3e19143c45c9394945ee7d7bce77bbfeb027c585
BobsunPower <bobsunpower@gmail.com>
2022-01-17 19:32:00 +0200

    Completed

--- lists_advanced/exercise/11. Inventory.py
+++ lists_advanced/exercise/11. Inventory.py
@@ -1,18 +1,16 @@
 lst = input().split(", ")
 while True:
-    cmd = input().split(" - ")
-    if cmd[0] == "Craft!":
+    cmd = input()
+    if cmd == "Craft!":
+        print(", ".join([str(i) for i in lst]))
         break
-    act, itm = cmd[0], cmd[1]
-    if act == "Collect":
-        itm not in lst and lst.append(itm)
-    elif act == "Drop":
-        itm in lst and lst.remove(itm)
+    act, itm = cmd.split(" - ")
+    if act == "Collect" and itm not in lst:
+        lst.append(itm)
+    elif act == "Drop" and itm in lst:
+        lst.remove(itm)
     elif act == "Combine Items":
         old, new = itm.split(":")
         old in lst and lst.insert(lst.index(old) + 1, new)
-    elif act == "Renew":
-        if itm in lst:
-            lst.remove(itm)
-            lst.append(itm)
-print(", ".join(lst))
+    elif act == "Renew" and itm in lst:
+        lst.append(lst.pop(lst.index(itm)))
