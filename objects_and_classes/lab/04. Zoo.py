596f8b65a2501e36cc4f369dd004525342e5bdf9
BobsunPower <bobsunpower@gmail.com>
2022-01-30 18:25:11 +0200

    Completed

--- objects_and_classes/lab/04. Zoo.py
+++ objects_and_classes/lab/04. Zoo.py
@@ -1,17 +1,17 @@
 class Zoo:
-    def __init__(self, alias):
-        self.name, self.animals = alias, {"mammal": [], "fish": [], "bird": []}
+    def __init__(self, name):
+        self.name, self.dic = name, {"mammal": [], "fish": [], "bird": []}
 
-    def add_animals(self, sps, alias):
-        self.animals[sps].append(alias)
+    def add_animals(self, sps, name):
+        self.dic[sps].append(name)
 
     def get_info(self, sps):
-        kind = {"mammal": "Mammals", "fish": "Fishes", "bird": "Birds"}
-        return f"{kind[sps]} in {self.name}: {', '.join(self.animals[sps])}"
+        dic = {"mammal": "Mammals", "fish": "Fishes", "bird": "Birds"}
+        return f"{dic[sps]} in {self.name}: {', '.join(self.dic[sps])}"
 
 
 zoo, tot = Zoo(input()), int(input())
 for i in range(tot):
-    species, name = input().split()
-    zoo.add_animals(species, name)
+    species, animal = input().split()
+    zoo.add_animals(species, animal)
 print(f"{zoo.get_info(input())}\nTotal animals: {tot}")
