f93b6694e8db3521bda197027a6320df94296dc8
BobsunPower <bobsunpower@gmail.com>
2022-01-31 18:27:19 +0200

    Completed

--- objects_and_classes/exercise/03. Catalogue.py
+++ objects_and_classes/exercise/03. Catalogue.py
@@ -1,7 +1,6 @@
 class Catalogue:
     def __init__(self, name):
-        self.name = name
-        self.products = []
+        self.name, self.products = name, []
 
     def add_product(self, product):
         self.products.append(product)
@@ -13,3 +12,13 @@ class Catalogue:
         self.products.sort()
         items = "\n".join(self.products)
         return f"Items in the {self.name} catalogue:\n{items}"
+
+
+catalogue = Catalogue("Furniture")
+catalogue.add_product("Sofa")
+catalogue.add_product("Mirror")
+catalogue.add_product("Desk")
+catalogue.add_product("Chair")
+catalogue.add_product("Carpet")
+print(catalogue.get_by_letter("C"))
+print(catalogue)
