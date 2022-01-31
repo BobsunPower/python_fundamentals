2bdfa934439448bc3e0b92534faad0ba59dd5b1e
BobsunPower <bobsunpower@gmail.com>
2022-01-31 18:43:58 +0200

    Completed

--- objects_and_classes/exercise/03. Catalogue.py
+++ objects_and_classes/exercise/03. Catalogue.py
@@ -9,16 +9,5 @@ class Catalogue:
         return [i for i in self.products if i.startswith(first_letter)]
 
     def __repr__(self):
-        self.products.sort()
-        items = "\n".join(self.products)
+        items = "\n".join(sorted(self.products))
         return f"Items in the {self.name} catalogue:\n{items}"
-
-
-catalogue = Catalogue("Furniture")
-catalogue.add_product("Sofa")
-catalogue.add_product("Mirror")
-catalogue.add_product("Desk")
-catalogue.add_product("Chair")
-catalogue.add_product("Carpet")
-print(catalogue.get_by_letter("C"))
-print(catalogue)
