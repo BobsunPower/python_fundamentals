4b6fea94c58721d2c15c3c287dd8bd6422977f42
BobsunPower <bobsunpower@gmail.com>
2022-02-02 22:30:04 +0200

    Completed

--- dictionaries/lab/02. Stock.py
+++ dictionaries/lab/02. Stock.py
@@ -1,3 +1,4 @@
-lst, sch = input().split(), input().split()
-dic = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
+lst, sch, pdt, qty = input().split(), input().split(), [], []
+[pdt.append(j) if i % 2 == 0 else qty.append(int(j)) for i, j in enumerate(lst)]
+dic = {k: v for k, v in zip(pdt, qty)}
 [print(f"We have {dic[i]} of {i} left") if i in dic else print(f"Sorry, we don't have {i}") for i in sch]
