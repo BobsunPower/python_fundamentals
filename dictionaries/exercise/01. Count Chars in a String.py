8cc3b482ad49cddf41a4aeb296ceb4a898db6015
BobsunPower <bobsunpower@gmail.com>
2022-02-04 00:17:24 +0200

    Completed

--- dictionaries/exercise/01. Count Chars in a String.py
+++ dictionaries/exercise/01. Count Chars in a String.py
@@ -1,15 +1,6 @@
 from collections import defaultdict
-dic, sch = defaultdict(list), 0
-while True:
-    cmd = input()
-    if cmd.isdigit():
-        sch = int(cmd)
-        break
-    key, num = cmd.split("-")
-    if key in dic:
-        dic[key] = num
-        continue
-    dic[key].append(num)
-for i in range(sch):
-    psn = input()
-    print(f"{psn} -> {''.join(dic[psn])}") if psn in dic else print(f"Contact {psn} does not exist.")
+txt, dic = input(), defaultdict(int)
+for i in txt:
+    if not i.isspace():
+        dic[i] += 1
+print("\n".join(f"{i} -> {j}" for i, j in dic.items()))
