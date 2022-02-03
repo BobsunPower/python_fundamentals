1843ba585a796ddea792f331aba96662a0bb14a5
BobsunPower <bobsunpower@gmail.com>
2022-02-03 09:04:50 +0200

    Completed

--- dictionaries/lab/03. Statistics.py
+++ dictionaries/lab/03. Statistics.py
@@ -1,10 +1,10 @@
 from collections import defaultdict
 dic = defaultdict(int)
 while True:
-    cmd = input().split(": ")
-    if cmd[0] == "statistics":
+    cmd = input()
+    if cmd == "statistics":
         break
-    itm, vlu = cmd[0], int(cmd[1])
-    dic[itm] += vlu
-print("Products in stock:"), [print(f"- {i}: {j}") for i, j in dic.items()]
+    key, qty = cmd.split(": ")
+    dic[key] += int(qty)
+print(f"Products in stock:"), [print(f"- {k}: {v}") for k, v in dic.items()]
 print(f"Total Products: {len(dic)}\nTotal Quantity: {sum(dic.values())}")
