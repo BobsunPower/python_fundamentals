594ae43df31880a6f7d3419dfbf8da5e499c3da5
BobsunPower <bobsunpower@gmail.com>
2022-02-07 15:12:25 +0200

    Completed

--- dictionaries/exercise/10. Company Users.py
+++ dictionaries/exercise/10. Company Users.py
@@ -1,15 +1,13 @@
-out = {}
+from collections import defaultdict
+dic = defaultdict(list)
 while True:
-    data = input()
-    if data == "End":
+    cmd = input()
+    if cmd == "End":
         break
-    firm, emp = data.split(" -> ")
-    if firm not in out:
-        out[firm] = []
-        out[firm].append(emp)
-    else:
-        if emp not in out[firm]:
-            out[firm].append(emp)
-for firm, emp in sorted(out.items(), key=lambda x: x[0], reverse=False):
-    print(f"{firm}")
-    print('\n'.join([f"-- {i}" for i in emp]))
+    com, emp = cmd.split(" -> ")
+    if emp in dic[com]:
+        continue
+    dic[com].append(emp)
+for c in dic:
+    print(f"{c}")
+    [print(f"-- {e}") for e in dic[c]]
