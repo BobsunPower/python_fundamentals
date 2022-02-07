43aa150a6a4b2aecb2e70cdd7b881ee9061bce2d
BobsunPower <bobsunpower@gmail.com>
2022-02-07 14:52:38 +0200

    Completed

--- dictionaries/exercise/09. Student Academy.py
+++ dictionaries/exercise/09. Student Academy.py
@@ -1,10 +1,5 @@
 from collections import defaultdict
-from statistics import mean
-dic, out, std = defaultdict(list), defaultdict(list), None
+dic = defaultdict(list)
 for i in range(int(input())):
-    std, grd = input(), float(input())
-    dic[std].append(grd)
-for i, j in dic.items():
-    if mean(j) >= 4.5:
-        out[i] = mean(j)
-[print(f"{i} -> {j:.2f}") for i, j in sorted(out.items(), key=lambda x: x[1], reverse=True)]
+    dic[input()].append(float(input()))
+[print(f"{k} -> {sum(v) / len(v):.2f}") for k, v in dic.items() if sum(v) / len(v) >= 4.5]
