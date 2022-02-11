bfeb6e63f67e8be615ccf31c5c57c4cd3f501fb4
BobsunPower <bobsunpower@gmail.com>
2022-02-11 12:42:46 +0200

    Completed

--- text_processing/exercise/01. Valid Usernames.py
+++ text_processing/exercise/01. Valid Usernames.py
@@ -1,8 +1,7 @@
-lst = input().split(", ")
-for pas in lst:
-    if 3 <= len(pas) <= 16:
-        for ch in pas:
-            if not (ch.isalpha() or ch.isdigit() or ch == "-" or ch == "_"):
+for i in input().split(", "):
+    if 3 <= len(i) <= 16:
+        for j in i:
+            if not (j.isdigit() or j.isalpha() or j == "-" or j == "_"):
                 break
         else:
-            print(pas)
+            print(i)
