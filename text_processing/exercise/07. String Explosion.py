17baf32b5fd9eb5e665bdfca3305b635c104c34a
BobsunPower <bobsunpower@gmail.com>
2022-02-13 10:26:59 +0200

    Completed

--- text_processing/exercise/07. String Explosion.py
+++ text_processing/exercise/07. String Explosion.py
@@ -1,19 +1,11 @@
-# TODO
-text = list(input())
-power = 0
-index = 0
-last_list = []
-for index in range(len(text)):
-    if text[index] == ">":
-        power += int(text[index + 1])
-        while power > 0 and index < (len(text) - 1):
-            if not text[index + 1] == ">":
-                index += 1
-                text[index] = "remove"
-                power -= 1
-            else:
-                break
-for el in text:
-    if el != "remove":
-        last_list.append(el)
-print(*last_list, sep="")
+txt, pwr, i = input(), 0, 0
+while i < len(txt):
+    if txt[i] == ">":
+        pwr += int(txt[i + 1])
+    else:
+        if pwr > 0:
+            txt = txt[:i] + txt[i + 1:]
+            pwr -= 1
+            i -= 1
+    i += 1
+print(txt)
