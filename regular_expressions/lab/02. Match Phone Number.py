524e9f471c6a9a69b2f4c742bf91754a0afc71a9
BobsunPower <bobsunpower@gmail.com>
2022-02-15 15:26:35 +0200

    Completed

--- regular_expressions/lab/02. Match Phone Number.py
+++ regular_expressions/lab/02. Match Phone Number.py
@@ -1,3 +1,3 @@
 import re
 pat = r'(\+359([- ])2\2\d{3}\2\d{4}\b)'
-print(', '.join([p for p, s in re.findall(pat, input())]))
+print(', '.join([p for p, _ in re.findall(pat, input())]))
--- regular_expressions/lab/03. Match Dates.py
+++ regular_expressions/lab/03. Match Dates.py
@@ -1,10 +1,3 @@
-# TODO
 import re
-
-dates = input()
-
-pattern = r"\b(\d{2})([/\.-])([A-Z][a-z]{2})\2(\d{4})\b"
-valid_dates = re.findall(pattern, dates)
-
-for valid_date in valid_dates:
-    print(f"Day: {valid_date[0]}, Month: {valid_date[2]}, Year: {valid_date[3]}")
+pat = r'(\d{2})([/\.-])([A-Z][a-z]{2})\2(\d{4})'
+[print(f"Day: {i[0]}, Month: {i[2]}, Year: {i[3]}") for i in re.findall(pat, input())]
